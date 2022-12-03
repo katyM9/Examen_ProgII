5d5f7046a7e9663848ac237cc2f5cc6cfa6c8ab7
katyM9 <114771093+katyM9@users.noreply.github.com>
2022-12-02 21:23:31 -0600

    Guardar, abrir
    Añadí la función de abrir archivos desde mi aplicación y también añadí la función de guardar archivos de tipo .txt.

---
+++ Examen_ProgII/Examen_ProgII/src/Editor_texto/Archivos_katy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Editor_texto;
+
+/**
+ *
+ * @author katy alvarado
+ */
+class Archivos_katy {
+//    
+//    public static void main(String[] args) {
+//        interfaz ventana = new interfaz();
+//        ventana.setVisible(true);
+    }
+
